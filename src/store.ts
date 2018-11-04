0311274dda9a512a0a7e58c599bbf29188dc7d09
llluper <mitchelljstanford@gmail.com>
2018-11-04 22:07:24 +1100

    Loans data structure added to StateType, mock loans data added to state

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,40 @@ Vue.use(Vuex);
 
 interface StateType {
   totalPayout: number;
+  loans: {
+    balance: number;
+    interest: number;
+    earlyPaymentFee: number;
+    payout: number;
+    topUp: boolean;
+  }[];
 }
 
 const state: StateType = {
   totalPayout: 0,
+  loans: [
+    {
+      balance: 100,
+      interest: 1,
+      earlyPaymentFee: 10,
+      payout: 111,
+      topUp: false,
+    },
+    {
+      balance: 200,
+      interest: 2,
+      earlyPaymentFee: 10,
+      payout: 212,
+      topUp: false,
+    },
+    {
+      balance: 300,
+      interest: 3,
+      earlyPaymentFee: 10,
+      payout: 313,
+      topUp: false,
+    },
+  ],
 };
 
 const mutations = {};
