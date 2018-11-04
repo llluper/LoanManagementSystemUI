c061c0710636fa1260bf12798495eda7aec42adf
llluper <mitchelljstanford@gmail.com>
2018-11-04 19:01:36 +1100

    Setup store structure and removed any unnecessary code or files

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,14 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const state = {};
+const mutations = {};
+const actions = {};
+const getters = {};
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state,
+  mutations,
+  actions,
+  getters,
 });
