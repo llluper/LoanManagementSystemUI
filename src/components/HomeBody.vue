cad70a13f76210219bda8b6fa3a840ce480f9f98
llluper <mitchelljstanford@gmail.com>
2018-11-04 22:10:36 +1100

    HomeBody component created (where the Loans component is rendered)

---
+++ src/components/HomeBody.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="homebody row">
+      <div class="column eight-twelfths">
+        <div class="loan-count text-medium">You have 0 Personal loans</div>          
+        <Loans />
+      </div>
+      <div class="column four-twelfths">
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Loans from './Loans.vue';
+
+export default Vue.extend({
+  name: 'homebody',
+  components: { Loans },
+});
+</script>
+
+<style lang="scss" scoped>
+.homebody {
+  padding-top: 30px;
+}
+.loan-count {
+  padding: 10px 0 50px;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HomeTop />
+    <HomeBody />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HomeTop from '../components/HomeTop.vue';
+import HomeBody from '../components/HomeBody.vue';
 
 export default Vue.extend({
   name: 'home',
   components: {
     HomeTop,
+    HomeBody,
   },
 });
 </script>
